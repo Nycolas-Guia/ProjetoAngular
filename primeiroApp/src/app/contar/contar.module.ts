e60a3562b896ebf94a9e42bc7b390970add736ce
Nycolas da Guia Santos <nycolas.guia@hotmail.com>
2020-05-16 19:39:18 +0000

    criando interface do somar

--- primeiroApp/src/app/contar/contar.module.ts
+++ primeiroApp/src/app/contar/contar.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { SomarComponent } from './component/somar';
+import { SomarComponent } from './component/somar/somar.component';
 
 
 
